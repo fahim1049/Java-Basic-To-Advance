d54b9dcbd25f97baff72d450a71dc4122a3eb348
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-11-08 14:56:57 +0600

    Escape Sequence Use In Java

---
+++ EscapeSequence.java
@@ -0,0 +1,10 @@
+public class EscapeSequence {
+    public static void main(String[] args){
+        System.out.println("My name Is \t Fahim"); //print with tab
+        System.out.println("Name \be Fahim");//backspace remove
+        System.out.println("Fahim is \ngood boy");
+        System.out.println("Fahim is a good boy \r He is 24 years old");//return
+        System.out.println("Fahim ID  \'221-35-1049\'");//single quote
+        System.out.println("\"Fahim\"");//double quote
+    }
+}
