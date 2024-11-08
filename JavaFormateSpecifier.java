eb56787d9a4627961fff7099c807222709ea562d
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-11-08 19:06:23 +0600

    Java Format Specifier

---
+++ JavaFormateSpecifier.java
@@ -0,0 +1,14 @@
+public class JavaFormateSpecifier {
+    public static void main(String[]args){
+        int a = 1049;
+        float f = 1049.32f;//in java float value with add (last  f )must.
+        double d = 221.35;
+
+        System.out.printf("Integer %d\n",a);
+        System.out.printf("float %.2f\n",f);
+        System.out.printf("double %.3f\n",d);
+/*
+We can fix print value with (%.2f)
+ */
+    }
+}
