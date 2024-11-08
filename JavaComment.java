85a04248012248cbe856945989edd298c8491551
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-11-08 14:58:01 +0600

    Java Comment Format

---
+++ JavaComment.java
@@ -0,0 +1,19 @@
+public class JavaComment {
+
+    public static void main(String[]args){
+
+        //single line comment
+        //display my name
+
+        System.out.println("Fahim");
+
+        //multiple line comment
+
+        /*disply my name
+
+        my name and University
+         */
+
+        System.out.println("Dffodil International University");
+    }
+}
