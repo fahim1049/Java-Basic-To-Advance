b5da50f57f13e7d46344194d77f1f8363e8e0ce7
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-11-10 21:40:14 +0600

    Assignment  Operator

---
+++ AssignmentOperator.java
@@ -0,0 +1,16 @@
+public class AssignmentOperator {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z;
+
+        x+=y;
+        System.out.println("X = " + x );
+
+        z = y;
+
+        System.out.println("Z is :" +z);
+
+    }
+}
