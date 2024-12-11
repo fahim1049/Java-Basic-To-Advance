a764cb0516c447f7f8ffb0099d705e1f634ba4e0
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-12-11 15:02:03 +0600

    Unary Operator

---
+++ UnaryOperator.java
@@ -0,0 +1,15 @@
+public class UnaryOperator {
+    public static void main(String[] args) {
+
+        int n = 30;
+        int y = 40;
+        int result1,result2;
+
+        result1 = -n;
+        System.out.println("result = " +result1);
+
+        result2 = +40;
+        System.out.println("result = " +result2);
+    }
+
+}
