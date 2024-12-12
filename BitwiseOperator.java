702d988a1fb3187be02e22d671d5fdb3d108a570
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-12-12 16:44:49 +0600

    Bitwise Operator

---
+++ BitwiseOperator.java
@@ -0,0 +1,21 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+
+        int c;
+
+        c = a & b;
+        System.out.println("a & b = " +c);
+
+        c = a|b;
+
+        System.out.println("a | b = " +c);
+
+        c = a^b;
+
+        System.out.println("a^b = " +c);
+        
+
+    }
+}
