580aef13fa0309f5cec9296f433ba8ad9bed43ff
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-12-12 21:44:23 +0600

    Array

---
+++ Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args) {
+        int [] number = new int[5];
+
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        number[3] = 4;
+        number[4] = 5;
+
+        int sum = number[0] + number[1] + number[2] + number[3] + number[4];
+        System.out.println("sum = " +sum);
+    }
+}
