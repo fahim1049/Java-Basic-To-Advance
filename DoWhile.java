4eeb6a693edf351cf0c4011b7afffd32a7b6650c
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-12-12 19:38:16 +0600

    Do While Loop Print Sentence

---
+++ DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int  i = 1;
+        do{
+            System.out.println("Fahim Is An Good Programmer");
+            i++;
+        }while(i<=10);
+    }
+}
