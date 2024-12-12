4c15acf2ca3b28c5b7dce8b4867b53dd2bc757a0
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-12-12 20:08:57 +0600

    find sum of 1-10 using for loop

---
+++ Assignment10.java
@@ -0,0 +1,14 @@
+
+// find sum of 1-10 using for loop
+
+public class Assignment10 {
+    public static void main(String[] args) {
+        int result = 0;
+        for(int i=1;i<=10;i++){
+           result = i+result;
+
+        }
+
+        System.out.println(result);
+    }
+}
