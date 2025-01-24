48d04796de8bf76c48ab065beb84c47d85f0cfe1
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-24 20:57:00 +0600

    Java String

---
+++ StringOne.java
@@ -0,0 +1,14 @@
+public class StringOne {
+    public static void main(String[]args){
+        String s1  ="Fahim Nur";
+        String s2 = new String ("Fahim Nur");
+         char[] s3 = { 'f','a','h','i','m'};
+
+
+        System.out.println("s1 = " +s1);
+        System.out.println("s2 = "+s2);
+        System.out.println(s3);
+
+
+    }
+}
