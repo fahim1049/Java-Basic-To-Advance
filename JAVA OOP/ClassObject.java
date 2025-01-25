284125a1b63e037cca21d66ffc549a6cda7d55e0
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-26 01:08:49 +0600

    Class and Object

---
+++ JAVA OOP/ClassObject.java
@@ -0,0 +1,20 @@
+public class ClassObject {
+    String name, gender;
+    int phone;
+
+
+        public static void main(String[] args) {
+            ClassObject teacher = new ClassObject();
+            teacher.name = "Fahim Nur";
+            teacher.gender = "Male";
+            teacher.phone = 0160000000;
+
+            System.out.println(teacher.name);
+            System.out.println(teacher.gender);
+            System.out.println(teacher.phone);
+
+        }
+    }
+
+
+
