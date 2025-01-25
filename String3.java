05e3635fe89840bc47e12731179938236125653a
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-26 00:14:58 +0600

    Length Of String

---
+++ String3.java
@@ -0,0 +1,12 @@
+public class String3 {
+    public static void main (String [] args){
+         String s1 = "Fahim";
+         String s2 =  new String ("Fahim Nur");
+
+         System.out.println("s1 =" +s1);
+        System.out.println("s2 = " +s2);
+
+        int len  = s1.length();
+        System.out.println("Length of s1 = " +len);
+    }
+}
