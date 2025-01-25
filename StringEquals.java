92eec7faa48bb74a72be70580bbab9392f57dac5
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-26 00:26:16 +0600

    String are Equal or Not

---
+++ StringEquals.java
@@ -0,0 +1,21 @@
+
+
+public class StringEquals {
+    public static void main(String [] args){
+        String s1 = "Fahim";
+        String s2 = "Fahim nur";
+
+        System.out.println("s1 = " +s1);
+        System.out.println("s2 = "+s2);
+
+        System.out.println("Now Decide Below Equal or Not ");
+
+        if(s1 == s2){
+            System.out.println(" Answer Equal");
+        }
+        else{
+            System.out.println(" Answer Not Equal");
+        }
+    }
+
+}
