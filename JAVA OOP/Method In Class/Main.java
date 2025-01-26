aee67a827f8cd02394c25d5cc1cacf81fc07a28e
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-26 19:28:18 +0600

    Method inside Class

---
+++ JAVA OOP/Method In Class/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Method teacher1 = new Method();
+        teacher1.name = "Fahim Nur";
+        teacher1.gender = "Male";
+        teacher1.phone = 016000000;
+       teacher1.displayInformation();
+
+        Method teacher2 = new Method();
+        teacher2.name = "Tonni";
+        teacher2.gender = "Female";
+        teacher2.phone = 01600;
+        teacher2.displayInformation();
+
+    }
+}
---
+++ JAVA OOP/Method In Class/Method.java
@@ -0,0 +1,14 @@
+public class Method {
+    String name,gender;
+    int phone;
+
+    void displayInformation(){
+        System.out.println("Name = " +name);
+        System.out.println("Gender = "+gender);
+        System.out.println("Phone Number = "+phone);
+
+        }
+
+    }
+
+
