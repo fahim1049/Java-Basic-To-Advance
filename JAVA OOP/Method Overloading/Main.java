20e8060040c3aa237d1307513ba58451482b56b2
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-26 23:05:20 +0600

    Method Overloading

---
+++ JAVA OOP/Method Overloading/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Teacher obj1 = new Teacher();
+        obj1.add(5,10);
+        obj1.add(5,20);
+        obj1.add(10,20,30);
+        obj1.add();
+}
+}
---
+++ JAVA OOP/Method Overloading/Teacher.java
@@ -0,0 +1,19 @@
+
+public class Teacher {
+
+  void add(int a,int b,int c){
+      System.out.println(a+b+c);
+  }
+
+  void add(double a,double b){
+      System.out.println("a+b");
+  }
+
+  void add(int a,double b){
+      System.out.println(a+b);
+  }
+
+  void add(){
+      System.out.println("Empty");
+  }
+}
