a4a1fe072ed1e101a864de85a409a41db7c47b04
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-26 20:13:21 +0600

    Constructor in Java

---
+++ JAVA OOP/Constructor/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Method teacher1 = new Method("Fahim Nur","Male",01756);
+
+        teacher1.displayInformation();
+
+        Method teacher2 = new Method("Tonni","Female",0160);
+
+        teacher2.displayInformation();
+
+    }
+}
---
+++ JAVA OOP/Constructor/Method.java
@@ -0,0 +1,23 @@
+public class Method {
+    String name,gender;
+    int phone;
+
+    Method(String n,String g,int p){
+
+        name = n;
+        gender =  g;
+        phone  = p;
+
+
+    }
+
+    void displayInformation(){
+        System.out.println("Name = " +name);
+        System.out.println("Gender = "+gender);
+        System.out.println("Phone Number = "+phone);
+
+        }
+
+    }
+
+
