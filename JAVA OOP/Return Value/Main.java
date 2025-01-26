4bb48008362126aabc79963fb932a9ae68c9a98b
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-26 22:11:16 +0600

    Return a value from method

---
+++ JAVA OOP/Return Value/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Teacher ob1 = new Teacher();
+        int result = ob1.squre(5);
+        System.out.println("Squre = " +result);
+    }
+    }
+
+
+
+
+
---
+++ JAVA OOP/Return Value/Teacher.java
@@ -0,0 +1,8 @@
+
+public class Teacher {
+
+    int squre(int value) {
+
+        return value * value;
+    }
+}
