767ed8ec6bf4165bfb7a8f68e53f1ab0bbc64701
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-27 13:37:48 +0600

    Encapsulation with getter and setter method

---
+++ JAVA OOP/Encapsulation/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+       Person p1 = new Person();
+       p1.setName("Fahim Nur");
+        System.out.println(p1.getName());
+        p1.setAge(24);
+        System.out.println(p1.getAge());
+
+    }
+}
