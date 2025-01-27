2278c1924a2bc6ddc81de33ad4d41a0631d5d0db
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-01-27 19:30:47 +0600

    Inheritance in Java

---
+++ JAVA OOP/Inheritance/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Teacher t1 = new Teacher();
+
+        t1.name = "Fahim nur";
+        t1.age = 24;
+        t1.qualification = "Msc In SWE";
+        t1.displayInformation2();
+
+        Teacher t2 = new Teacher();
+
+        t2.name = "Tonni";
+        t2.age = 28;
+        t2.qualification = "Bsc";
+        t2.displayInformation2();
+
+
+    }
+}
---
+++ JAVA OOP/Inheritance/Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher extends Person {
+
+    String qualification;
+
+    void displayInformation2(){
+    displayInformation1();
+        System.out.println("Qualification: "+qualification);
+    }
+
+}
